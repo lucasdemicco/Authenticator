7ef1df8e7fb069ff6933bcd4f66b8d7ac123c0e0
lucas <lucasmicco@hotmail.com>
2025-07-21 15:15:26 -0300

    Add Dockerfile Render V

--- Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
+++ Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2586872cfe764b63cb46386db4c297423295f53b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe6fd1b9d321b23c5df57c9e7674a63fde6bf546")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
